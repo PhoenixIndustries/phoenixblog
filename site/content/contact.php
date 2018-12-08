e4bb281080ff1306bfff9425db62ab55657f7446
Philip Müller <philm@manjaro.org>
2018-12-08 14:04:31 +0100

    Update contact.php

--- site/content/contact.php
+++ site/content/contact.php
@@ -14,7 +14,7 @@ $subject = '[Manjaro] Support Request';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'need' => 'Need', 'email' => 'Email', 'message' => 'Message'); 
 
 // message that will be displayed when everything is OK :)
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+$okMessage = 'Contact form successfully submitted. Thank you, we will get back to you soon!';
 
 // If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
