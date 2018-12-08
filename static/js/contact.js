49ff7e260714d47289f9d8955941e12659839c1e
Philip Müller <philm@manjaro.org>
2018-12-08 13:35:18 +0100

    Update contact.js for server

--- static/js/contact.js
+++ static/js/contact.js
@@ -12,7 +12,7 @@ $(function () {
 
         // if the validator does not prevent form submit
         if (!e.isDefaultPrevented()) {
-            var url = "http://localhost:1314/contact.php";
+            var url = "contact.php";
 
             // POST values in the background the the script URL
             $.ajax({
