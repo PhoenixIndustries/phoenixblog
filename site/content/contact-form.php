226b1fd6b891ee42c623e42669e5d920a0da04c6
Vitor Lopes <vlopes@gmail.com>
2019-05-09 15:39:36 +0100

    finish php, add ajax

--- site/content/contact-form.php
+++ site/content/contact-form.php
@@ -1,89 +1,81 @@
 <?php
-/*
- *  CONFIGURE EVERYTHING HERE
- */
+    
+if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST') {
 
-// an email address that will receive the email with the output of the form
-$sendTo = 'Manjaro Support <vmnlop@gmail.com>';
+    $sendTo = 'Manjaro Support <test@email.com>';
+    $subject = '[Manjaro] Support Request';
+    $fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); 
+    $success_message = 'Message successfully submitted. Thank you, we will get back to you soon!';
 
-// subject of the email
-$subject = '[Manjaro] Support Request';
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
 
-// form field names and their translations.
-// array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'need' => 'Need', 'email' => 'Email', 'message' => 'Message'); 
+    // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+    error_reporting(E_ALL & ~E_NOTICE);
 
-// message that will be displayed when everything is OK :)
-$okMessage = 'Contact form successfully submitted. Thank you, we will get back to you soon!';
+    try {
 
-// If something goes wrong, we will display this message.
-$errorMessage = 'There was an error while submitting the form. Please try again later';
+        if(count($_POST) == 0) throw new \Exception('Form is empty');
+                
+        $emailText = "From: manjaro.org\n=============================================\n";
 
-/*
- *  LET'S DO THE SENDING
- */
-
-// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-error_reporting(E_ALL & ~E_NOTICE);
-
-try
-{
-
-    if(count($_POST) == 0) throw new \Exception('Form is empty');
-            
-    $emailText = "You have a new message from your contact form\n=============================================\n";
-
-    foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email 
-        if (isset($fields[$key])) {
-            $emailText .= "$fields[$key]: $value\n";
-            if ( $fields[$key] == "Email" ) {
-                $fromEmail = $value;
+        foreach ($_POST as $key => $value) {
+            test_input($value);
+            // If the field exists in the $fields array, include it in the email 
+            if (isset($fields[$key])) {
+                $emailText .= "$fields[$key]: $value\n";
+                if ( $fields[$key] == "Email" ) {
+                    $fromEmail = $value;
+                    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        $error_message = "Invalid email format"; 
+                        $responseArray = array('type' => 'error', 'message' => $error_message);
+                      }
+                }
+                if ( $fields[$key] == "Name" ) {
+                    $fromName = $value;
+                    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+                        $error_message = "Only letters and white space allowed in name field"; 
+                        $responseArray = array('type' => 'error', 'message' => $error_message);
+                      }
+                }
             }
-            if ( $fields[$key] == "Name" ) {
-                $fromName = $value;
-            }
-            if ( $fields[$key] == "Surname" ) {
-                $fromSurname = $value;
-            }
+
+            // an email address that will be in the From field of the email.
+            $from = "$fromName <$fromEmail>";
         }
 
-        // an email address that will be in the From field of the email.
-        $from = "$fromName $fromSurname <$fromEmail>";
+        // All the neccessary headers for the email.
+        $headers = array('Content-Type: text/plain; charset="UTF-8";',
+            'From: ' . $from,
+            'Reply-To: ' . $from,
+            'Return-Path: ' . $from,
+        );
+        
+        
+        if(empty($error_message)){
+            mail($sendTo, $subject, $emailText, implode("\n", $headers));
+            $responseArray = array('type' => 'success', 'message' => $success_message);
+
+        } else {
+            $responseArray = array('type' => 'error', 'message' => $error_message);
+            mail($sendTo, $subject, $emailText, implode("\n", $headers));
+        }
+
+    } catch (\Exception $e) {
+        $responseArray = array('type' => 'error', 'message' => "Something went wrong. Please try again later");
     }
 
-    // All the neccessary headers for the email.
-    $headers = array('Content-Type: text/plain; charset="UTF-8";',
-        'From: ' . $from,
-        'Reply-To: ' . $from,
-        'Return-Path: ' . $from,
-    );
-    
-    // Send email
-    mail($sendTo, $subject, $emailText, implode("\n", $headers));
-
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-}
-catch (\Exception $e)
-{
-    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-}
-
-
-// if requested by AJAX request return JSON response
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
-
     header('Content-Type: application/json');
-
     echo $encoded;
+
+} else {
+    header("HTTP/1.0 404 Not Found");
 }
-// else just display the message
-else {
-    echo '<script type="text/javascript">
-    window.location="http://localhost:1313" ;
-            alert("'.$responseArray['message'].'");
-                
-        </script>';
-}
+die();
+
 
