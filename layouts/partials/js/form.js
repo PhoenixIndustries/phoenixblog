489173b41b75f9ee1185ce1703448a1f68f7e82b
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2020-02-29 19:28:30 +0300

    fixed #154

--- layouts/partials/js/form.js
+++ layouts/partials/js/form.js
@@ -2,80 +2,85 @@
 $("#btn-contact").on('click', function() {
     var template = `
     <div id="contact-form" class="modal fade" tabindex="-1" role="dialog">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-            <form id="form" method="post" role="form" >
-    <div class="row">
-            <div class="col-md-6">
-            <span class="text-muted">All fields are required. If you are a user looking for support please use the <a href="https://forum.manjaro.org/" target="_blank">forum</a>.</span>
-            <br>
-            <br>
-            <label for="name">Name</label>
-            <input id="name" type="text" name="name" class="form-control" placeholder="Please enter your Name" required="required">   
-            <br>
-    </div>
-        <div class="g-recaptcha col-md-6" data-sitekey="{{ .Site.Params.recaptcha_public_key }}"></div>
-    </div>
-    <div class="row">
-        
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="subject">Subject</label>
-                <select id="subject" name="subject" class="form-control" required="required">
-                    <option value="Report Hardware Issue">Report Hardware Issue</option>
-                    <option value="Provide Feedback">Provide Feedback</option>
-                    <option value="Hardware Inquire">Hardware Inquire</option>
-                    <option value="Software Inquire">Software Inquire</option>
-                    <option value="Other">Other</option>
-                </select>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input id="email" type="email" name="email" class="form-control" placeholder="Please enter your Email" required="required">
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group">
-                <label for="message">Message</label>
-                <textarea id="message" name="message" class="form-control" placeholder="Leave a Message, We will get back to you as soon as we can." rows="4" required="required"></textarea>
-            </div>
-        </div>
-        <div class="col-md-12">
-            <input type="submit" class="btn btn-success btn-send" value="Send">
-        </div>
-    </div>
-    <div class="row">
-       
-    </div>
-</form>
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <form id="form" method="post" role="form">
+                <div class="row">
+                    <div class="col-md-12">
+                        <span class="text-muted">If you are a user looking for support please use the <a href="https://forum.manjaro.org/" target="_blank">forum</a>.</span>
 
-</div></div>
+                        <div class="form-group mt-3">
+                            <label for="name">Name</label>
+                            <input id="name" type="text" name="name" class="form-control" placeholder="Please enter your Name" required="required">
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row mt-3">
+
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="subject">Subject</label>
+                            <select id="subject" name="subject" class="form-control custom-select" required="required">
+                                <option value="Report Hardware Issue">Report Hardware Issue</option>
+                                <option value="Provide Feedback">Provide Feedback</option>
+                                <option value="Hardware Inquire">Hardware Inquire</option>
+                                <option value="Software Inquire">Software Inquire</option>
+                                <option value="Other">Other</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input id="email" type="email" name="email" class="form-control" placeholder="Please enter your Email" required="required">
+                            <small class="form-text text-muted">We'll never share your information with anyone else.</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="message">Message</label>
+                            <textarea id="message" name="message" class="form-control" placeholder="Leave a Message, We will get back to you as soon as we can." rows="4" required="required"></textarea>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row  mt-3">
+                    <div class="col-md-9">
+
+                        <div class="g-recaptcha" data-sitekey="{{ .Site.Params.recaptcha_public_key }}"></div>
+                    </div>
+                    <div class="col-md-3 mt-4">
+                        <button type="submit" class="btn btn-success btn-block">Send</button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </div>
 </div>
-    
     `
-$( "body" ).append( template );
-let script = document.createElement('script');
-script.src = "https://www.google.com/recaptcha/api.js";
-document.head.append(script);
-$('#form').submit(function(event) {
-    event.preventDefault();
+    $("body").append(template);
+    let script = document.createElement('script');
+    script.src = "https://www.google.com/recaptcha/api.js";
+    document.head.append(script);
+    $('#form').submit(function(event) {
+        event.preventDefault();
         var formData = $('#form').serialize();
         $.ajax({
-            type        : 'POST', 
-            url         : '{{ .Site.Params.form_server_address_hacked }}', 
-            data        : formData, 
-            dataType    : 'json',
-            encode      : true
+            type: 'POST',
+            url: '{{ .Site.Params.form_server_address_hacked }}',
+            data: formData,
+            dataType: 'json',
+            encode: true
         }).done(function(data) {
             if (data['type'] === 'success') {
                 $('#contact-form, .modal-backdrop').hide().remove();
             }
             alert(data['message']);
             console.log(data);
-            });
         });
+    });
 });
